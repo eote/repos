546e1555f58b1ab925b6d2fa7da0b08a5b40b4bd
Eotect Nahn <eotect@gmail.com>
2011-11-25 00:46:17 +0800

    Add generate-log.pl.

---
+++ generate-log.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/perl -w
+use strict;
+use Cwd qw/getcwd/;
+
+my $log_filename = 'repos.log';
+my @repos_type = qw/svn hg git/;
+my $CWD = getcwd();
+
+sub generate_log {
+	my $logfile = shift;
+	my $dir = shift;
+	open FO, ">", $logfile;
+	chdir $dir;
+	open FI,"-|","perl generate-log.pl";
+	print FO <FI>;
+	close FO;
+	chdir $CWD;
+}
+
+sub generate_diff {
+	my $filename = shift;
+	my $left = shift;
+	my $right = shift;
+	open FO,">",$filename;
+	open FI,"-|","diff",$left,$right;
+	print FO <FI>;
+	close FI;
+	close FO;
+}
+
+foreach(@repos_type) {
+	next unless(-d $_);
+	my $filename = "$_-$log_filename";
+	if( -f $filename) {
+		rename $filename, $filename . ".old";
+	}
+	generate_log($filename,$_);
+	if( -f $filename . ".old") {
+		generate_diff("$filename.diff",$filename , $filename . ".old");
+	}
+	else {
+		system("cp","--",$filename,"$filename.diff");
+	}
+}
+
