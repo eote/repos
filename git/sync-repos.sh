19c65c74f2ffdb788a5b23033c709d9bed0d5786
Eotect Nahn <eotect@gmail.com>
2011-12-03 15:56:54 +0800

    Update script git/sync-repos.sh.

--- git/sync-repos.sh
+++ git/sync-repos.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 script="reposman --to-remote"
 logfile="sync-repos.log"
+echo " "
 echo "========================================================" | tee -a $logfile
-echo $script "$@" | tee -a $logfile
 date +"[%Y-%m-%d %H:%M:%S] Started:" | tee -a $logfile
+echo $script "$@" | tee -a $logfile
+echo " "
 $script "$@" 2>&1 | tee -a $logfile
+echo " "
 date +"[%Y-%m-%d %H:%M:%S] Finished." | tee -a $logfile
-echo
+echo .
+echo " "
 
